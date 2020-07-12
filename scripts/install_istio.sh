a44a4490c7183dee5ea371c22d51552d1b802638
Tarun Pabbi <tarun.pabbi@fairfaxmedia.com.au>
2020-07-12 13:48:40 +1000

    updated install script

--- scripts/install_istio.sh
+++ scripts/install_istio.sh
@@ -1,5 +1,4 @@
-curl -L https://istio.io/downloadIstio | sh -
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.6.5 sh -
 cd istio-1.6.5
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo
-kubectl label namespace default istio-injection=enabled
